9938618452e5e5610cef20c33d071120caa4e091
taher salah <tahersalah2016@gmail.com>
2022-11-06 21:46:26 +0200

    add dotes and gateg

---
+++ lib/models/banner/banner_image.dart
@@ -0,0 +1,14 @@
+class BannerModel {
+  final String image;
+  // final String title;
+  // final String body;
+
+
+  BannerModel(this.image,);
+  
+  List <BannerModel> banners=[
+    BannerModel('assets/images/banner.png',),
+    BannerModel('assets/images/banner.png',),
+    BannerModel('assets/images/banner.png',),
+  ];
+}
